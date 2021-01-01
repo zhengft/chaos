c571ee070aea8fe65bc389fdfde77ce4dac7fb68
Feiteng Zheng <zhengft_hz@qq.com>
2021-01-01 17:20:56 +0800

    update python/xml/lxml/test_pretty_print.py

--- python/xml/lxml/test_pretty_print.py
+++ python/xml/lxml/test_pretty_print.py
@@ -9,9 +9,11 @@ def test_pretty_print_01():
     child2 = etree.SubElement(root, "child2")
     child3 = etree.SubElement(root, "child3")
 
-    result = etree.tostring(root, pretty_print=True).decode()
+    r0 = etree.tostring(root, pretty_print=True)
+    assert isinstance(r0, bytes)
 
-    assert isinstance(result, str)
+    r1 = r0.decode()
+    assert isinstance(r1, str)
     expect = """
 <root>
   <child1/>
@@ -19,4 +21,4 @@ def test_pretty_print_01():
   <child3/>
 </root>
 """
-    assert expect.lstrip() == result
+    assert expect.lstrip() == r1
